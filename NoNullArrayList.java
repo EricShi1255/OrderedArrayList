e958c5a7c533c3489d84b8d4d9d53c803dc669f8
EricShi1255 <eshi20@stuy.edu>
2020-12-13 12:47:18 -0500

    skeleton framework for methods

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T> {
+    private ArrayList<T> data;
     
+    //constructor; no arguments
+    public NoNullArrayList() {
+        data = new ArrayList<T>();
+    }
+    public NoNullArrayList(int n) {
+        data = new ArrayList<T>(n);
+    }
+    public boolean add(int index, T element) {
+        if (element == null) {
+            throw new IllegalArgumentException("Null values cannot be added");
+        }    
+        else {
+            data.add(index, element);
+        }
+    }
+    public T set(int index, T value)  {
+
+    }
 }
