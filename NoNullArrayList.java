1a4cd0a1c79a6bde835b5be9b3982952a3603463
EricShi1255 <eshi20@stuy.edu>
2020-12-13 13:40:30 -0500

    constructors + add done, having problems with set

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,23 +1,38 @@
 import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T> {
-    private ArrayList<T> data;
     
     //constructor; no arguments
     public NoNullArrayList() {
-        data = new ArrayList<T>();
+        super();
     }
-    public NoNullArrayList(int n) {
-        data = new ArrayList<T>(n);
+    public NoNullArrayList(int startingCapacity) {
+        super(startingCapacity);
     }
-    public boolean add(int index, T element) {
+    public boolean add(T element) {
         if (element == null) {
             throw new IllegalArgumentException("Null values cannot be added");
-        }    
-        else {
-            data.add(index, element);
         }
+        else {
+            super.add(element);
+            return true;
+        } 
     }
-    public T set(int index, T value)  {
+    public void add(int index, T element) {
+        if (element == null) {
+            throw new IllegalArgumentException("Null values cannot be added");
+        }
+        else {
+            super.add(index, element);
+        } 
+    }
 
+    public T set(int index, T value)  {
+        if (value == null) {
+            throw new IllegalArgumentException("Null values cannot be added");
+        }
+        else {
+        super.set(index, value);
+        return value;
+        }
     }
 }
